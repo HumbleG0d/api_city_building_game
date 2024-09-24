72590f8b0b81e849a44b0e85e4bbd0d84f6dab51
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-23 21:27:26 -0500

    Add initial commit

---
+++ api/src/test/java/com/buil/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buil.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
