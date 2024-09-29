ac6242324cd29885a0c6126afe6caf15b462cfce
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-29 16:56:45 -0500

    Add the method for conversion to string

--- api/src/main/java/com/buil/api/model/infraestructure/TypeInfraestructure.java
+++ api/src/main/java/com/buil/api/model/infraestructure/TypeInfraestructure.java
@@ -3,5 +3,14 @@ package com.buil.api.model.infraestructure;
 public enum TypeInfraestructure {
     RANCH,
     BUILDING,
-    FACTORY
+    FACTORY;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+
+    public static TypeInfraestructure fromString(String value) {
+        return TypeInfraestructure.valueOf(value.toUpperCase()); 
+    }
 }
