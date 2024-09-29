cd32697b6801c8a19c91196ee8d330439e159556
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-29 16:57:55 -0500

    Change return Infraestructure to List

--- api/src/main/java/com/buil/api/repository/InfraestructureRepository.java
+++ api/src/main/java/com/buil/api/repository/InfraestructureRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InfraestructureRepository extends JpaRepository<Infraestructure, Long> {
-  Infraestructure findByName(String name);
+  List<Infraestructure> findByName(String name);
   List<Infraestructure> findInfraestructureByTypeInfraestructure(TypeInfraestructure typeInfraestructure);
+
 }
