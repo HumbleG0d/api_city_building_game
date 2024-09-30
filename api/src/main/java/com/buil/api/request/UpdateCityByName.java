6a54f284cc61561c4dd49f916d588946c8b14637
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-30 18:01:11 -0500

    Add documentation request

--- api/src/main/java/com/buil/api/request/UpdateCityByName.java
+++ api/src/main/java/com/buil/api/request/UpdateCityByName.java
@@ -3,7 +3,15 @@ package com.buil.api.request;
 import lombok.Data;
 
 @Data
+/**
+ * Request object for updating a city by its name.
+ * This class contains the necessary data to identify and modify a city's details.
+ */
 public class UpdateCityByName {
+
+    // Unique identifier for the city
     private Long id;
+
+    // New name for the city
     private String name;
 }
--- api/src/main/java/com/buil/api/request/UpdatePopulationRequest.java
+++ api/src/main/java/com/buil/api/request/UpdatePopulationRequest.java
@@ -2,15 +2,34 @@ package com.buil.api.request;
 
 import lombok.Data;
 
+
+/**
+ * Represents a request to update the population details of a city.
+ * This class encapsulates the data required to modify the attributes 
+ * related to the city's population.
+ */
 @Data
 public class UpdatePopulationRequest {
-  private int total_inhabitants;
-  private double growth;
-  private double pbi;
 
-  public UpdatePopulationRequest(int total_inhabitants, double growth, double pbi) {
-    this.total_inhabitants = total_inhabitants;
-    this.growth = growth;
-    this.pbi = pbi;
-  }
+    // Total number of inhabitants in the city
+    private int total_inhabitants;
+    
+    // Growth rate of the population
+    private double growth;
+    
+    // Productive output of the city (PBI - Producto Bruto Interno)
+    private double pbi;
+
+    /**
+     * Constructs a new UpdatePopulationRequest with the specified values.
+     *
+     * @param total_inhabitants the total number of inhabitants
+     * @param growth the growth rate of the population
+     * @param pbi the productive output of the city
+     */
+    public UpdatePopulationRequest(int total_inhabitants, double growth, double pbi) {
+        this.total_inhabitants = total_inhabitants;
+        this.growth = growth;
+        this.pbi = pbi;
+    }
 }
