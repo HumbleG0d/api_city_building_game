1e1996af1d07ec376c3210d6f0e640c71efaf48e
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-30 17:55:57 -0500

    Add documentation dtos

--- api/src/main/java/com/buil/api/dto/CityDTO.java
+++ api/src/main/java/com/buil/api/dto/CityDTO.java
@@ -10,11 +10,29 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+/**
+ * Data Transfer Object (DTO) for transferring City data between processes.
+ * This DTO includes information about the city's ID, name, resources, population, coins, and infrastructures.
+ */
 public class CityDTO {
+    
+    // Unique identifier for the city
     private Long id;
+
+    // Name of the city
     private String name;
+
+    // List of resources available in the city
     private List<ResourceDTO> resource;
+
+    // Total population in the city
     private int totalPopulation;
+
+    // Total amount of coins the city possesses (economic balance)
     private BigDecimal coins;
+
+    // List of infrastructures present in the city
     private List<InfraestructureDTO> infraestructures;
+
+    // Getters and setters omitted for brevity
 }
