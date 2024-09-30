1c07056b1f982f7e899a15a75d78dbb1a366c360
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-29 22:04:20 -0500

    Add repository documentation

--- api/src/main/java/com/buil/api/repository/CityRepository.java
+++ api/src/main/java/com/buil/api/repository/CityRepository.java
@@ -3,6 +3,18 @@ package com.buil.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.buil.api.model.City;
 
-public interface CityRepository extends JpaRepository<City,Long> {
+
+/**
+ * Repository interface for managing {@link City} entities.
+ * Extends JpaRepository to provide CRUD operations and custom queries for City entities.
+ */
+public interface CityRepository extends JpaRepository<City, Long> {
+
+    /**
+     * Retrieves a city by its name.
+     *
+     * @param name the name of the city to find
+     * @return the {@link City} with the specified name, or null if no city is found
+     */
     City findByName(String name);
 }
