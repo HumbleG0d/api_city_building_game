a8ec670c4918c792d33a62f23a9b85928a79cffd
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-09-30 18:07:58 -0500

    Add documentation response

--- api/src/main/java/com/buil/api/response/ApiResponse.java
+++ api/src/main/java/com/buil/api/response/ApiResponse.java
@@ -3,9 +3,18 @@ package com.buil.api.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * A generic class that represents a standard API response structure.
+ * It encapsulates a message and the data returned by an API call.
+ *
+ * @param <T> the type of the data being returned in the response.
+ */
 @Data
 @AllArgsConstructor
 public class ApiResponse<T> {
+      /**
+     * A message describing the outcome of the API call (e.g., success, error).
+     */
     private String message;
     private T data;
 }
